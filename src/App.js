272a6bf1a0709893cdac3d5f6a4ccfa69f2ec20f
cenxun111 <32427222+cenxun111@users.noreply.github.com>
2021-06-24 15:38:10 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+    return (
+        <div>
+            <h1 className='text-blue-500 text-3xl justify-center'>hellogagnicegagdaga this nice sg world</h1>
+            <button className="bg-transparent bg-green-500 text-3xl rounded-md items-center">nice</button>
+        </div>
+    )
+}
+
+export default App;
